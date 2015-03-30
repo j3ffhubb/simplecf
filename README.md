e5c79f679d5d4c00d8f5b29e93a9ff132a52a834
MusiKernel Team <musikernel@nospam.org>
2015-03-30 19:54:57 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+#Warning, pre-alpha software, don't use
+
+#What is simplecf?
+
+simplecf is a templating system for AWS Cloudformation, similar to Cumulus.
+
+#Why not use Cumulus?
+
+Cumulus is good, but I prefer something based on a more traditional templating language like Mustache, that doesn't attempt to ensure the source template still builds something valid even without Cumulus (a bad design constraint IMHO).  Cumulus accepts Mustache-style tags, but can only accept them as environment variables, and not in the Yaml files.  A pure templating-language approach is much simpler, and much easier to understand.  Rather than using 2 different data formats (JSON and YAML), simplecf uses only JSON.
+
+#How to use
+
+Create a standard AWS Cloudformation template, adding Mustache {{ tags }} for the parts you wish to be dynamically substituted
+
+```
+...
+"Tags":[
+  {"Key": "Phase", "Value": "{{ phase }}"}
+]
+...
+```
+Next, create a JSON file to define the stack and fill in the tags
+
+```
+{
+  "STACK_NAME": "MyStackProd",
+  "STACK_REGION": "us-east-1",
+  "phase": "Prod"
+}
+```
+
+Then run simplecf to generate the template
+
+`simplecf.py -t my_cf_template.json -d prod.json`
+
---
+++ bin/simplecf.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+"""
+This file is part of the simplecf project, Copyright simplecf Team
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; version 3 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+"""
+
+import os
+import pystache
+import json
+import argparse
+
+def json_clean(path):
+    check_file_exists(path)
+    with open(path) as file_handle:
+        raise NotImplementedError
+
+def json_load(path):
+    check_file_exists(path)
+    with open(path) as file_handle:
+        return json.load(file_handle)
+
+def check_file_exists(path):
+    if not os.path.isfile(path):
+        print("Error:  {0} does not exist".format(path))
+        exit(1)
+
+def read_file_text(path):
+    check_file_exists(path)
+    with open(path) as file_handle:
+        return file_handle.read()
+
+def write_file_text(path, text):
+    with open(path, "w") as file_handle:
+        file_handle.write(text)
+
+def validate_data_file(path):
+    raise NotImplementedError
+
+def escape_template(cf_template, data_file):
+    cf_text = read_file_text(cf_template)
+    data_json = json_load(data_file)
+    result = pystache.render(cf_text, data_json)
+    outfile = "{0}.json".format(data_json["STACK_NAME"])
+    write_file_text(outfile, result)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--template", dest="template")
+    parser.add_argument("-d", "--data-file", dest="data_file")
+    args = parser.parse_args()
+    if not args.template or args.data_file:
+        print("Error:  Must specify -t and -d")
+        exit(1)
+
+main()
